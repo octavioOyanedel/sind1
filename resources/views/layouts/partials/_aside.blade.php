6c854a6fd953767e8b7df103f654be0ecf01db4b
Octavio Oyanedel <octavio.oyanedel@gmail.com>
2021-02-03 15:05:15 -0300

    Finalizado módulo socio, en proceso revisión y testeo módulo socios.

--- resources/views/layouts/partials/_aside.blade.php
+++ resources/views/layouts/partials/_aside.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
     <!-- LOGO -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{route('home')}}" class="brand-link">
         <img src="\img\logo_verde.png" alt="Logo SIND1" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SIND1</span>
     </a>
